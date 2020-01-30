2d9b09aab8f444056a927d15fc55e3f0a87c1c23
Shyam Dwaraknath <shyamd@lbl.gov>
2020-01-30 15:18:31 -0800

    add search

--- src/mp_api/xas/client.py
+++ src/mp_api/xas/client.py
@@ -35,3 +35,33 @@ class XASRESTer(RESTer):
 
         return self._make_request(query_string)
 
+    def search_xas_docs(
+        self,
+        edge: Optional[Edge] = None,
+        absorbing_element: Optional[Element] = None,
+        required_elements: Optional[List[Element]] = None,
+        formula: Optional[str] = None,
+        chemsys: Optional[str] = None,
+        skip: Optional[PositiveInt] = 0,
+        limit: Optional[PositiveInt] = 10,
+    ):
+        query_params = {
+            "edge": f"{edge.value}" if edge else None,
+            "absorbing_element": f"{str(absorbing_element)}"
+            if absorbing_element
+            else None,
+            "elements": "&".join([f"elements={str(el)}" for el in required_elements])
+            if required_elements
+            else None,
+            "formula": formula,
+            "chemsys": chemsys,
+            "skip": skip,
+            "limit": limit,
+        }
+
+        query_params = {k: v for k, v in query_params.items() if v is not None}
+
+        query_string = "&".join([f"{k}={v}" for k, v in query_params.items()])
+
+        return self._make_request(f"search?{query_string}")
+
