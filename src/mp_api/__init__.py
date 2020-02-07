6d749ead42547a2e9d2d08ea16d46326e7d8cab7
Shyam Dwaraknath <shyamd@lbl.gov>
2020-02-07 12:17:15 -0800

    add version to package

---
+++ src/mp_api/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+""" Primary MAPI module """
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
