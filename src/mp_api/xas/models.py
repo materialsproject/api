e273304511f8d8ca82987867c237720c4eeae18f
Shyam Dwaraknath <shyamd@lbl.gov>
2020-02-16 11:34:29 -0800

    use default None's in model to allow for sparse fields

--- src/mp_api/xas/models.py
+++ src/mp_api/xas/models.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, Field
 
 from pymatgen import Element
 
-from mp_api.core.models import Structure, Composition
+from mp_api.materials.models import Structure, Composition
 
 
 class Edge(Enum):
@@ -39,60 +39,60 @@ class XASSpectrum(BaseModel):
     Spectrum Data
     """
 
-    x: List[float] = Field(..., title="X-ray energy")
-    y: List[float] = Field(..., title="Absorption (Arbitrary Units)")
+    x: List[float] = Field(None, title="X-ray energy")
+    y: List[float] = Field(None, title="Absorption (Arbitrary Units)")
     structure: Optional[Structure] = Field(None, title="Structure")
-    absorbing_element: Optional[Element] = Field(..., title="Absoring Element")
+    absorbing_element: Optional[Element] = Field(None, title="Absoring Element")
     edge: Edge = Field(
-        ..., title="Absorption Edge", description="The interaction edge for XAS"
+        None, title="Absorption Edge", description="The interaction edge for XAS"
     )
 
 
 class XASDoc(BaseModel):
-    spectrum: XASSpectrum
+    spectrum: XASSpectrum = None
 
     edge: Edge = Field(
-        ..., title="Absorption Edge", description="The interaction edge for XAS"
+        None, title="Absorption Edge", description="The interaction edge for XAS"
     )
-    absorbing_element: Element = Field(..., title="Absoring Element")
+    absorbing_element: Element = Field(None, title="Absoring Element")
 
     xas_ids: List[str] = Field(
-        ...,
+        None,
         title="XAS IDs",
         description="List of FEFF Calculations IDS used to make this XAS spectrum",
     )
     task_id: str = Field(
-        ...,
+        None,
         title="Task ID",
         description="The task ID for the material this spectrum corresponds to",
     )
 
     # Metadata from structure
-    elements: List[Element] = Field(..., description="List of elements in the material")
-    nelements: int = Field(..., title="Number of Elements")
+    elements: List[Element] = Field(None, description="List of elements in the material")
+    nelements: int = Field(None, title="Number of Elements")
     composition: Composition = Field(
-        ..., description="Full composition for the material"
+        None, description="Full composition for the material"
     )
     composition_reduced: Composition = Field(
-        ...,
+        None,
         title="Reduced Composition",
         description="Simplified representation of the composition",
     )
     formula_pretty: str = Field(
-        ..., title="Pretty Formula", description="Cleaned representation of the formula"
+        None, title="Pretty Formula", description="Cleaned representation of the formula"
     )
     formula_anonymous: str = Field(
-        ...,
+        None,
         title="Anonymous Formula",
         description="Anonymized representation of the formula",
     )
-    chemsys: str = Field(
-        ...,
+    chemsys: Optional[str] = Field(
+        None,
         title="Chemical System",
         description="dash-delimited string of elements in the material",
     )
     last_updated: datetime = Field(
-        ...,
+        None,
         description="timestamp for the most recent calculation for this XAS spectrum",
     )
 
@@ -103,12 +103,12 @@ class XASSearchResponse(BaseModel):
     """
 
     task_id: str = Field(
-        ...,
+        None,
         title="Task ID",
         description="The task ID for the material this spectrum corresponds to",
     )
-    formula_pretty: str = Field(..., description="Pretty Formula for the material")
+    formula_pretty: str = Field(None, description="Pretty Formula for the material")
     edge: Edge = Field(
-        ..., title="Absorption Edge", description="The interaction edge for XAS"
+        None, title="Absorption Edge", description="The interaction edge for XAS"
     )
-    absorbing_element: Element = Field(..., title="Absoring Element")
+    absorbing_element: Element = Field(None, title="Absoring Element")
