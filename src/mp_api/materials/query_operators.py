811f939345c24609f398cf95e3b97a56ef341395
Shyam Dwaraknath <shyamd@lbl.gov>
2020-02-16 11:34:10 -0800

    conver dependency to query operator

--- src/mp_api/materials/query_operators.py
+++ src/mp_api/materials/query_operators.py
@@ -1,28 +1,22 @@
-from typing import Optional, Callable
+from typing import Optional
 from fastapi import Query
-from mp_api.core.dependencies import STORE_PARAMS
+from mp_api.core.query_operator import STORE_PARAMS, QueryOperator
 from mp_api.materials.utils import formula_to_criteria
 
 
-def formula_query_factory() -> Callable[..., STORE_PARAMS]:
+class FormulaQuery(QueryOperator):
     """
     Factory method to generate a dependency for querying by formula with wild cards
-
-    Args:
-        default_skip: default number of items to skip
-        default_limit: default number of items to return
-        max_limit: max number of items to return
     """
 
     def query(
+        self,
         formula: Optional[str] = Query(
             None,
             description="Query by formula including anonymized formula or by including wild cards",
-        )
+        ),
     ) -> STORE_PARAMS:
         """
         Pagination parameters for the API Endpoint
         """
         return {"criteria": formula_to_criteria(formula)} if formula else {}
-
-    return query
