8280ad8838f29f303cfb231548dff63ae0c74a78
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-05 21:02:48 -0800

    Load main app in import

--- src/mp_api/__init__.py
+++ src/mp_api/__init__.py
@@ -1,9 +1,22 @@
 # coding: utf-8
 """ Primary MAPI module """
 from pkg_resources import get_distribution, DistributionNotFound
+from monty.serialization import loadfn
 
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
     # package is not installed
+    pass
+
+from mp_api.core.settings import MAPISettings
+
+try:
+    settings = MAPISettings()
+    mapi = loadfn(settings.app_path)
+    app = mapi.app
+except Exception as e:
+    # Something went wrong with loading default app
+    print("Failed loadning App")
+    print(e)
     pass
