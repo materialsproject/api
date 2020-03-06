a9295f0c15deedfc72ccb01413b1a72ee3de7c8a
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-05 18:04:15 -0800

    remove app

---
+++ src/mp_api/core/settings.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings, Field
+
+
+class MAPISettings(BaseSettings):
+    """
+    Special class to store settings for MAPI
+    python module
+    """
+
+    app_path: str = Field(
+        "~/mapi.json", description="Path for the default MAPI JSON definition"
+    )
+
+    class Config:
+        env_prefix = "mapi_"
