c974f0ab8a1386aa1dde0fd2de6ec82ae77ad2e4
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-06 09:28:20 -0800

    update app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from monty.serialization import loadfn, dumpfn
+from monty.serialization import loadfn
 from fastapi import FastAPI
 from mp_api.xas.models import XASDoc
 
@@ -8,25 +8,33 @@ from mp_api.materials.query_operators import FormulaQuery
 from mp_api.xas.query_operator import XASQuery
 
 from mp_api.core.resource import Resource
+from mp_api.core.api import MAPI
 
 xas_store = os.environ.get("XAS_STORE", "xas_store.json")
 xas_store = loadfn(xas_store)
 
-xas_resource = Resource(
-    xas_store,
-    XASDoc,
-    query_operators=[
-        FormulaQuery(),
-        XASQuery(),
-        PaginationQuery(),
-        SparseFieldsQuery(
+api = MAPI(
+    resources={
+        "xas": Resource(
+            xas_store,
             XASDoc,
-            default_fields=["task_id", "edge", "absorbing_element", "last_updated"],
-        ),
-    ],
+            query_operators=[
+                FormulaQuery(),
+                XASQuery(),
+                PaginationQuery(),
+                SparseFieldsQuery(
+                    XASDoc,
+                    default_fields=[
+                        "xas_id",
+                        "edge",
+                        "absorbing_element",
+                        "spectrum_type",
+                        "last_updated",
+                    ],
+                ),
+            ],
+        )
+    }
 )
 
-dumpfn(xas_resource, "xas_resource.json")
-
-app = FastAPI(title="Materials Project API", version="3.0.0-dev")
-app.include_router(xas_resource.router)
+app = api.app
