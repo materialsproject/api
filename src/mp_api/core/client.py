1be0f4a1b0f98f7c5884a98ce7620f35c4e67ee5
Shyam Dwaraknath <shyamd@lbl.gov>
2020-03-12 18:29:17 -0700

    better RESTER error messages

--- src/mp_api/core/client.py
+++ src/mp_api/core/client.py
@@ -66,9 +66,7 @@ class RESTer:
             else:
 
                 raise RESTError(
-                    "REST query returned with error status code {}".format(
-                        response.status_code
-                    )
+                    "REST query returned with error status code {response.status_code} on url {url}"
                 )
 
         except Exception as ex:
