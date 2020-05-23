faab53a2ebd3e106b753b4bc24922ade6ddc8988
munrojm <jmunro@lbl.gov>
2020-05-22 17:05:14 -0700

    Added client method for generic search

--- src/mp_api/materials/client.py
+++ src/mp_api/materials/client.py
@@ -1,9 +1,10 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from pymatgen import Element
 from pymatgen import Structure
 
 from mp_api.core.client import RESTer, RESTError
 from mp_api.xas.models import Edge, XASType
+from mp_api.materials.models.core import CrystalSystem
 
 
 class CoreRESTer(RESTer):
@@ -17,8 +18,15 @@ class CoreRESTer(RESTer):
         super().__init__(endpoint=self.api_url + "/core/", **kwargs)
 
     def get_structure_from_material_id(self, material_id: str):
-        query_params = {"fields": ["structure"]}
+        """
+        Get a structure for a given Materials Project ID.
 
+        Arguments:
+            material_id (str): Materials project ID
+
+        Returns:
+            structure (Structure): Pymatgen structure object 
+        """
         result = self._make_request("{}/?fields=structure".format(material_id))
 
         if len(result.get("data", [])) > 0:
@@ -26,3 +34,79 @@ class CoreRESTer(RESTer):
             return structure
         else:
             raise RESTError("No document found")
+
+    def search_material_docs(
+        self,
+        chemsys_formula: Optional[str] = None,
+        crystal_system: Optional[CrystalSystem] = None,
+        spacegroup_number: Optional[int] = None,
+        spacegroup_symbol: Optional[str] = None,
+        nsites: Optional[Tuple[int, int]] = (None, None),
+        volume: Optional[Tuple[float, float]] = (None, None),
+        density: Optional[Tuple[float, float]] = (None, None),
+        deprecated: Optional[bool] = False,
+        limit: Optional[int] = 10,
+        skip: Optional[int] = 0,
+        fields: Optional[List[str]] = [None],
+    ):
+        """
+        Query core material docs using a variety of search criteria.
+
+        Arguments:
+            chemsys_formula (str): A chemical system (e.g., Li-Fe-O),
+                or formula including anonomyzed formula
+                or wild cards (e.g., Fe2O3, ABO3, Si*).
+            crystal_system (CrystalSystem): Crystal system of material.
+            spacegroup_number (int): Space group number of material.
+            spacegroup_symbol (str): Space group symbol of the material in international short symbol notation.
+            nsites (Tuple[int,int]): Minimum and maximum number of sites to consider. 
+            volume (Tuple[float,float]): Minimum and maximum volume to consider. 
+            density (Tuple[float,float]): Minimum and maximum density to consider. 
+            deprecated (bool): Whether the material is tagged as deprecated.
+            limit (int): Maximum number of structures to return.
+            skip (int): Number of entries to skip in the search.
+            fields (List[str]): List of fields in MaterialsCoreDoc to return data for.
+                Default is material_id, last_updated, and formula_pretty.
+
+        Returns:
+            ([dict]) List of dictionaries containing Materials Project IDs
+                reduced chemical formulas, and last updated tags.
+        """
+
+        query_params = {"limit": limit, "skip": skip, "deprecated": deprecated}
+        if chemsys_formula:
+            if "-" in chemsys_formula:
+                eles = chemsys_formula.replace("-", ",")
+                query_params.update({"elements": eles})
+            else:
+                query_params.update({"formula": chemsys_formula})
+
+        query_params.update(
+            {
+                "crystal_system": crystal_system,
+                "spacegroup_number": spacegroup_number,
+                "crystal_system": crystal_system,
+            }
+        )
+
+        if any(nsites):
+            query_params.update({"nsites_min": nsites[0], "nsites_max": nsites[1]})
+
+        if any(volume):
+            query_params.update({"volume_min": volume[0], "volume_max": volume[1]})
+
+        if any(density):
+            query_params.update({"density_min": density[0], "density_max": density[1]})
+
+        if any(fields):
+            query_params.update({"fields": ",".join(fields)})
+
+        query_params = {
+            entry: query_params[entry]
+            for entry in query_params
+            if query_params[entry] is not None
+        }
+        print(query_params)
+        results = self.query(query_params)
+
+        return results.get("data", [])
