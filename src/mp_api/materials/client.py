d01b99426c8002b4b3e418e843cac5b36918e113
munrojm <jmunro@lbl.gov>
2020-07-07 14:15:24 -0700

    Core rester search method is now a generator

--- src/mp_api/materials/client.py
+++ src/mp_api/materials/client.py
@@ -35,7 +35,7 @@ class CoreRESTer(RESTer):
     def search_material_docs(
         self,
         chemsys_formula: Optional[str] = None,
-        task_ids: Optional[List[str]] = None,
+        task_ids: Optional[List[str]] = [None],
         crystal_system: Optional[CrystalSystem] = None,
         spacegroup_number: Optional[int] = None,
         spacegroup_symbol: Optional[str] = None,
@@ -43,8 +43,8 @@ class CoreRESTer(RESTer):
         volume: Optional[Tuple[float, float]] = (None, None),
         density: Optional[Tuple[float, float]] = (None, None),
         deprecated: Optional[bool] = False,
-        limit: Optional[int] = 10,
-        skip: Optional[int] = 0,
+        num_chunks: Optional[int] = None,
+        chunk_size: Optional[int] = 100,
         fields: Optional[List[str]] = [None],
     ):
         """
@@ -54,7 +54,7 @@ class CoreRESTer(RESTer):
             chemsys_formula (str): A chemical system (e.g., Li-Fe-O),
                 or formula including anonomyzed formula
                 or wild cards (e.g., Fe2O3, ABO3, Si*).
-            task_ids (List[str]): List of IDs to return data for.
+            task_ids (List[str]): List of Materials Project IDs to return data for.
             crystal_system (CrystalSystem): Crystal system of material.
             spacegroup_number (int): Space group number of material.
             spacegroup_symbol (str): Space group symbol of the material in international short symbol notation.
@@ -62,17 +62,18 @@ class CoreRESTer(RESTer):
             volume (Tuple[float,float]): Minimum and maximum volume to consider.
             density (Tuple[float,float]): Minimum and maximum density to consider.
             deprecated (bool): Whether the material is tagged as deprecated.
-            limit (int): Maximum number of structures to return.
-            skip (int): Number of entries to skip in the search.
+            num_chunks (int): Maximum number of chunks of data to yield. None will yield all possible.
+            chunk_size (int): Number of data entries per chunk.
             fields (List[str]): List of fields in MaterialsCoreDoc to return data for.
                 Default is material_id, last_updated, and formula_pretty.
 
-        Returns:
-            ([dict]) List of dictionaries containing Materials Project IDs
-                reduced chemical formulas, and last updated tags.
+        Yields:
+            ([dict]) List of dictionaries containing data for entries defined in 'fields'. 
+                Defaults to Materials Project IDs reduced chemical formulas, and last updated tags.
         """
 
-        query_params = {"limit": limit, "skip": skip, "deprecated": deprecated}
+        query_params = {"deprecated": deprecated}
+
         if chemsys_formula:
             query_params.update({"formula": chemsys_formula})
 
@@ -105,6 +106,14 @@ class CoreRESTer(RESTer):
             if query_params[entry] is not None
         }
 
-        results = self.query(query_params)
+        query_params.update({"limit": chunk_size, "skip": 0})
+        count = 0
+        while True:
+            query_params["skip"] = count * chunk_size
+            results = self.query(query_params).get("data", [])
 
-        return results.get("data", [])
+            if not any(results) or (num_chunks is not None and count == num_chunks):
+                break
+
+            count += 1
+            yield results
