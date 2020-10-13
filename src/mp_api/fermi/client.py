87ed64a94d034e956a4280a50542866869d94b15
munrojm <jmunro@lbl.gov>
2020-10-12 20:37:53 -0700

    Search method added to fermi rester

--- src/mp_api/fermi/client.py
+++ src/mp_api/fermi/client.py
@@ -1,4 +1,6 @@
 from mp_api.core.client import BaseRester, MPRestError
+from collections import defaultdict
+from typing import Optional, List
 
 
 class FermiRester(BaseRester):
@@ -22,3 +24,46 @@ class FermiRester(BaseRester):
             return result
         else:
             raise MPRestError("No document found")
+
+    def search_fermi_docs(
+        self,
+        num_chunks: Optional[int] = None,
+        chunk_size: int = 100,
+        fields: Optional[List[str]] = None,
+    ):
+        """
+        Query fermi docs using a variety of search criteria.
+
+        Arguments:
+            num_chunks (int): Maximum number of chunks of data to yield. None will yield all possible.
+            chunk_size (int): Number of data entries per chunk.
+            fields (List[str]): List of fields in EOSDoc to return data for.
+                Default is material_id and last_updated only.
+
+        Yields:
+            ([dict]) List of dictionaries containing data for entries defined in 'fields'.
+                Defaults to Materials Project IDs only.
+        """
+
+        query_params = defaultdict(dict)  # type: dict
+
+        if fields:
+            query_params.update({"fields": ",".join(fields)})
+
+        query_params = {
+            entry: query_params[entry]
+            for entry in query_params
+            if query_params[entry] is not None
+        }
+
+        query_params.update({"limit": chunk_size, "skip": 0})
+        count = 0
+        while True:
+            query_params["skip"] = count * chunk_size
+            results = self.query(query_params).get("data", [])
+
+            if not any(results) or (num_chunks is not None and count == num_chunks):
+                break
+
+            count += 1
+            yield results
