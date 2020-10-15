e794f5084363ae98147044d36d8ecda0854c6c8a
munrojm <jmunro@lbl.gov>
2020-10-15 13:56:27 -0700

    Substrates endpoint added

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ xas_store_json = os.environ.get("XAS_STORE", "xas_store.json")
 gb_store_json = os.environ.get("GB_STORE", "xas_store.json")
 fermi_store_json = os.environ.get("FERMI_STORE", "fermi_store.json")
 doi_store_json = os.environ.get("DOI_STORE", "doi_store.json")
+substrates_store_json = os.environ.get("SUBSTRATES_STORE", "substrates_store.json")
 
 bs_store_json = os.environ.get("BS_STORE", "bs_store.json")
 dos_store_json = os.environ.get("DOS_STORE", "dos_store.json")
@@ -88,6 +89,13 @@ if db_uri:
         collection_name="dois",
     )
 
+    substrates_store = MongoURIStore(
+        uri=f"mongodb+srv://{db_uri}",
+        database="mp_core",
+        key="film_id",
+        collection_name="substrates",
+    )
+
     bs_store = MongoURIStore(
         uri=f"mongodb+srv://{db_uri}",
         database="mp_core",
@@ -130,6 +138,7 @@ else:
     gb_store = loadfn(gb_store_json)
     fermi_store = loadfn(fermi_store_json)
     doi_store = loadfn(doi_store_json)
+    substrates_store = loadfn(substrates_store_json)
 
     bs_store = loadfn(bs_store_json)
     dos_store = loadfn(dos_store_json)
@@ -182,6 +191,11 @@ resources.update({"fermi": fermi_resource(fermi_store)})
 from mp_api.dois.resources import dois_resource
 
 resources.update({"doi": dois_resource(doi_store)})
+
+# Substrates
+from mp_api.substrates.resources import substrates_resource
+
+resources.update({"substrates": substrates_resource(substrates_store)})
 
 # Band Structure
 from mp_api.bandstructure.resources import bs_resource
