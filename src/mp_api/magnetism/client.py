c326b7d15a240359f25d2e2932ffecc5cf4b38e0
munrojm <jmunro@lbl.gov>
2020-10-29 14:00:26 -0700

    Magnetism endpoint added

---
+++ src/mp_api/magnetism/client.py
@@ -0,0 +1,101 @@
+from typing import List, Optional, Tuple
+from collections import defaultdict
+import warnings
+
+from mp_api.core.client import BaseRester, MPRestError
+from mp_api.magnetism.models import MagneticOrderingEnum, TotalMagNormalizationEnum
+
+
+class MagnetismRester(BaseRester):
+
+    suffix = "magnetism"
+
+    def get_magnetism_from_material_id(self, material_id: str):
+        """
+        Get magnetic data for a given Materials Project ID.
+
+        Arguments:
+            material_id (str): Materials project ID
+
+        Returns:
+            results (Dict): Dictionary containing magnetic data.
+        """
+
+        result = self._make_request("{}/?all_fields=true".format(material_id))
+
+        if len(result.get("data", [])) > 0:
+            return result
+        else:
+            raise MPRestError("No document found")
+
+    def search_magnetism_docs(
+        self,
+        ordering: Optional[MagneticOrderingEnum] = None,
+        total_magnetization: Optional[Tuple[float, float]] = None,
+        total_magnetization_normalization: Optional[TotalMagNormalizationEnum] = None,
+        num_chunks: Optional[int] = None,
+        chunk_size: int = 100,
+        fields: Optional[List[str]] = None,
+    ):
+        """
+        Query magnetism docs using a variety of search criteria.
+
+        Arguments:
+            ordering (MagneticOrderingEnum]): The magnetic ordering of the material.
+            total_magnetization (Tuple[float,float]): Minimum and maximum total magnetization values to consider.
+            total_magnetization_normalization (TotalMagNormalizationEnum): Type of normalization applied to values
+                of total_magnetization supplied.
+            num_chunks (int): Maximum number of chunks of data to yield. None will yield all possible.
+            chunk_size (int): Number of data entries per chunk.
+            fields (List[str]): List of fields in EOSDoc to return data for.
+                Default is material_id only.
+
+        Yields:
+            ([dict]) List of dictionaries containing data for entries defined in 'fields'.
+                Defaults to Materials Project IDs only.
+        """
+
+        query_params = defaultdict(dict)  # type: dict
+
+        if chunk_size <= 0 or chunk_size > 100:
+            warnings.warn("Improper chunk size given. Setting value to 100.")
+            chunk_size = 100
+
+        if total_magnetization:
+            query_params.update(
+                {
+                    "total_magnetization_min": total_magnetization[0],
+                    "total_magnetization_max": total_magnetization[1],
+                }
+            )
+
+        if total_magnetization_normalization:
+            query_params.update(
+                {
+                    "total_magnetization_normalization": total_magnetization_normalization.value,
+                }
+            )
+
+        if ordering:
+            query_params.update({"ordering": ordering.value})
+
+        if fields:
+            query_params.update({"fields": ",".join(fields)})
+
+        query_params = {
+            entry: query_params[entry]
+            for entry in query_params
+            if query_params[entry] is not None
+        }
+
+        query_params.update({"limit": chunk_size, "skip": 0})
+        count = 0
+        while True:
+            query_params["skip"] = count * chunk_size
+            results = self.query(query_params).get("data", [])
+
+            if not any(results) or (num_chunks is not None and count == num_chunks):
+                break
+
+            count += 1
+            yield results
