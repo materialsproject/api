799a44b303297be042605e8b7e2d31141d29f509
Matthew Horton <mkhorton@users.noreply.github.com>
2020-11-16 23:56:13 -0800

    MPRester client: add experimental document model support, fix default arg in BaseRester

--- src/mp_api/robocrys/client.py
+++ src/mp_api/robocrys/client.py
@@ -2,6 +2,7 @@ from typing import List, Optional, Tuple
 from collections import defaultdict
 
 from mp_api.core.client import BaseRester, MPRestError
+from mp_api.robocrys.models import RobocrysDoc
 
 import warnings
 
@@ -9,3 +10,4 @@ import warnings
 class RobocrysRester(BaseRester):
 
     suffix = "robocrys"
+    document_model = RobocrysDoc
