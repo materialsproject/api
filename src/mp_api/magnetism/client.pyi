8a573dd3e0e34c6b0a618a9dd770a52941a0953c
Matthew Horton <mkhorton@users.noreply.github.com>
2020-11-17 20:31:00 -0800

    Add MagnetismRester type hint

---
+++ src/mp_api/magnetism/client.pyi
@@ -0,0 +1,9 @@
+from typing import List, Optional
+from mp_api.magnetism.models import MagnetismDoc
+
+
+class MagnetismRester:
+    def query_by_task_id(
+        self, task_id, fields: Optional[List[str]] = None, monty_decode: bool = True
+    ) -> MagnetismDoc:
+        ...
