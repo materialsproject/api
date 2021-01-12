d11fede215c4b64a2b97fbff514527fd1675e67f
Jason Munro <jmunro@lbl.gov>
2021-01-12 14:19:57 -0800

    Split chgcar client functions

--- src/mp_api/charge_density/client.py
+++ src/mp_api/charge_density/client.py
@@ -8,30 +8,42 @@ class ChargeDensityRester(BaseRester):
 
     def get_charge_density_from_material_id(self, material_id: str):
         """
-        Get charge density data and calculations details for a given Materials Project ID.
+        Get charge density data for a given Materials Project ID.
 
         Arguments:
             material_id (str): Materials project ID
 
         Returns:
-            calc_data (dict): Dictionary containing calculation details and charge density data.
+            chgcar (dict): Pymatgen CHGCAR object.
         """
 
         result = self._make_request(
             "{}/?fields=data&all_fields=false".format(material_id)
         )
 
-        result = {"data": "test"}
-
         if result.get("data", None) is not None:
-            d = result["data"]
-            base_endpoint = "/".join(self.endpoint.split("/")[0:3])
-            task_rester = TaskRester(api_key=self.api_key, endpoint=base_endpoint)
-
-            c = task_rester.get_task_from_material_id(
-                material_id, fields=["task_id", "orig_inputs"]
-            ).get("data")[0]
-
-            return {**c, "chgcar": d}
+            return result["data"]
         else:
             raise MPRestError("No document found")
+
+    def get_calculation_details(self, material_id: str):
+        """
+        Get charge density calculations details for a given Materials Project ID.
+
+        Arguments:
+            material_id (str): Materials project ID
+
+        Returns:
+            calc_details (dict): Dictionary containing INCAR, POSCAR, and KPOINTS data for the DFT calculation.
+        """
+
+        base_endpoint = "/".join(self.endpoint.split("/")[0:3])
+        task_rester = TaskRester(api_key=self.api_key, endpoint=base_endpoint)
+
+        result = task_rester.get_task_from_material_id(
+            material_id, fields=["task_id", "orig_inputs"]
+        ).get("data")[0]
+
+        task_rester.session.close()
+
+        return result
