a2b698a39124c4ce5a7eeb7e48bb1eefec93eaa9
Jason Munro <jmunro@lbl.gov>
2021-04-29 14:45:13 -0700

    Dielectric transitioned to emmet models

--- src/mp_api/routes/dielectric/models.py
+++ src/mp_api/routes/dielectric/models.py
@@ -2,48 +2,9 @@ from typing import List
 from datetime import datetime
 from monty.json import MontyDecoder
 
+from emmet.core.polar import Dielectric
+
 from pydantic import BaseModel, Field, validator
-
-
-class DielectricData(BaseModel):
-    """
-    Model for dielectric data in dielectric document
-    """
-
-    total: List[List[float]] = Field(
-        None,
-        description="Total dielectric tensor",
-    )
-
-    ionic: List[List[float]] = Field(
-        None,
-        description="Ionic contribution to dielectric tensor",
-    )
-
-    static: List[List[float]] = Field(
-        None,
-        description="Electronic contribution to dielectric tensor",
-    )
-
-    e_total: float = Field(
-        None,
-        description="Total dielectric constant",
-    )
-
-    e_ionic: float = Field(
-        None,
-        description="Ionic contributio to dielectric constant",
-    )
-
-    e_static: float = Field(
-        None,
-        description="Electronic contribution to dielectric constant",
-    )
-
-    n: float = Field(
-        None,
-        description="Refractive index",
-    )
 
 
 class DielectricDoc(BaseModel):
@@ -51,9 +12,8 @@ class DielectricDoc(BaseModel):
     Model for a document containing dielectric data
     """
 
-    dielectric: DielectricData = Field(
-        None,
-        description="Dielectric data",
+    dielectric: Dielectric = Field(
+        None, description="Dielectric data",
     )
 
     task_id: str = Field(
