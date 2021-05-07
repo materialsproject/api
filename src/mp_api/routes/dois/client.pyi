e66cd8a5ed64cfef2c3ec3b347e3233775ba49cf
Jason Munro <jmunro@lbl.gov>
2021-05-07 13:47:18 -0700

    DOI client update

---
+++ src/mp_api/routes/dois/client.pyi
@@ -0,0 +1,13 @@
+from typing import List, Optional
+from mp_api.routes.dois.models import DOIDoc
+
+
+class DOIRester:
+
+    def get_document_by_id(
+        self,
+        document_id: str,
+        fields: Optional[List[str]] = None,
+        monty_decode: bool = True,
+        version: Optional[str] = None,
+    ) -> DOIDoc: ...
