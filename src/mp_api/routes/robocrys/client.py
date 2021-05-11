c8a2f931f4af490b0d6cb9822b9934ec2b74e173
Jason Munro <jmunro@lbl.gov>
2021-05-11 14:19:18 -0700

    Robocrys client update

--- src/mp_api/routes/robocrys/client.py
+++ src/mp_api/routes/robocrys/client.py
@@ -5,4 +5,25 @@ from mp_api.routes.robocrys.models import RobocrysDoc
 class RobocrysRester(BaseRester):
 
     suffix = "robocrys"
-    document_model = RobocrysDoc
+    document_model = RobocrysDoc  # type: ignore
+
+    def search_robocrys_text(self, keywords: list[str]):
+        """
+        Search text generated from Robocrystallographer.
+
+        Arguments:
+            keywords (List[str]): List of search keywords
+
+        Returns:
+            robocrys_docs (List[RobocrysDoc]): List of robocrystallographer documents
+        """
+
+        keyword_string = ",".join(keywords)
+
+        results = self._query_resource(
+            criteria={"keywords": keyword_string},
+            suburl="text_search",
+            use_document_model=True,
+        )
+
+        return results
