cc4bb782d757f8e429c259d159d376bb6861ba00
Jason Munro <jmunro@lbl.gov>
2021-05-11 16:56:50 -0700

    Task basic search method update

--- src/mp_api/routes/tasks/client.py
+++ src/mp_api/routes/tasks/client.py
@@ -36,7 +36,8 @@ class TaskRester(BaseRester):
         self,
         chemsys_formula: Optional[str] = None,
         num_chunks: Optional[int] = None,
-        chunk_size: int = 100,
+        chunk_size: int = 1000,
+        all_fields: bool = True,
         fields: Optional[List[str]] = None,
     ):
         """
@@ -48,35 +49,24 @@ class TaskRester(BaseRester):
                 or wild cards (e.g., Fe2O3, ABO3, Si*).
             num_chunks (int): Maximum number of chunks of data to yield. None will yield all possible.
             chunk_size (int): Number of data entries per chunk. Max size is 100.
+            all_fields (bool): Whether to return all fields in the document. Defaults to True.
             fields (List[str]): List of fields in MaterialsCoreDoc to return data for.
                 Default is material_id, last_updated, and formula_pretty.
 
-        Yields:
-            ([dict]) List of dictionaries containing data for entries defined in 'fields'.
-                Defaults to Materials Project IDs reduced chemical formulas, and last updated tags.
+        Returns:
+            ([TaskpDoc]) List of task documents
         """
 
         query_params = {}  # type: dict
 
-        if chunk_size <= 0:
-            warnings.warn("Improper chunk size given. Setting value to 100.")
-            chunk_size = 100
-
         if chemsys_formula:
             query_params.update({"formula": chemsys_formula})
 
-        if fields:
-            query_params.update({"fields": ",".join(fields)})
-
-        query_params.update({"limit": chunk_size, "skip": 0})
-        count = 0
-        while True:
-            query_params["skip"] = count * chunk_size
-            results = self._query_resource(query_params).get("data", [])
-
-            if not any(results) or (num_chunks is not None and count == num_chunks):
-                break
-
-            count += 1
-            for result in results:
-                yield result
+        return super().search(
+            version=self.version,
+            num_chunks=num_chunks,
+            chunk_size=chunk_size,
+            all_fields=all_fields,
+            fields=fields,
+            **query_params
+        )
