d2750cd8bffe6f95bbc60660d1c03beffb236d9d
Jason Munro <jmunro@lbl.gov>
2021-05-11 14:25:57 -0700

    Linting

--- src/mp_api/routes/similarity/client.py
+++ src/mp_api/routes/similarity/client.py
@@ -1,24 +1,6 @@
-from mp_api.core.client import BaseRester, MPRestError
+from mp_api.core.client import BaseRester
 
 
 class SimilarityRester(BaseRester):
 
     suffix = "similarity"
-
-    def get_similar_structures(self, material_id: str):
-        """
-        Get similar structures for a given Materials Project ID.
-
-        Arguments:
-            material_id (str): Materials project ID
-
-        Returns:
-            results (Dict): Dictionary containing structure similarity data.
-        """
-
-        result = self._make_request("{}/?all_fields=true".format(material_id))
-
-        if len(result.get("data", [])) > 0:
-            return result
-        else:
-            raise MPRestError("No document found")
