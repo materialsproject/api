39963ddb215b0714225b4e0ae0d3d7084709e253
Jason Munro <jmunro@lbl.gov>
2021-05-11 13:11:33 -0700

    Stubs for es client

---
+++ src/mp_api/routes/electronic_structure/client.pyi
@@ -0,0 +1,13 @@
+from typing import List, Optional
+from emmet.core.electronic_structure import ElectronicStructureDoc
+
+
+class ElectronicStructureRester:
+
+    def get_document_by_id(
+        self,
+        document_id: str,
+        fields: Optional[List[str]] = None,
+        monty_decode: bool = True,
+        version: Optional[str] = None,
+    ) -> ElectronicStructureDoc: ...
