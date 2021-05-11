6d592a1b2d0371cd325774df003459d8deb9c1d4
Jason Munro <jmunro@lbl.gov>
2021-05-11 13:50:17 -0700

    Phonon client update

--- src/mp_api/routes/phonon/client.py
+++ src/mp_api/routes/phonon/client.py
@@ -1,4 +1,4 @@
-from mp_api.core.client import BaseRester, MPRestError
+from mp_api.core.client import BaseRester
 from mp_api.routes.phonon.models import PhononBSDoc, PhononImgDoc
 
 
@@ -7,44 +7,8 @@ class PhononRester(BaseRester):
     suffix = "phonon"
     document_model = PhononBSDoc  # type: ignore
 
-    def get_phonon_from_material_id(self, material_id: str):
-        """
-        Get phonon band structure data for a given Materials Project ID.
-
-        Arguments:
-            material_id (str): Materials project ID
-
-        Returns:
-            results (Dict): Dictionary containing phonon band structure data.
-        """
-
-        result = self._make_request("{}/?all_fields=true".format(material_id))
-
-        if len(result.get("data", [])) > 0:
-            return result
-        else:
-            raise MPRestError("No document found")
-
 
 class PhononImgRester(BaseRester):
 
     suffix = "phonon_img"
     document_model = PhononImgDoc  # type: ignore
-
-    def get_phonon_img_from_material_id(self, material_id: str):
-        """
-        Get phonon band structure image for a given Materials Project ID.
-
-        Arguments:
-            material_id (str): Materials project ID
-
-        Returns:
-            results (Dict): Dictionary containing phonon band structure image data.
-        """
-
-        result = self._make_request("{}/?all_fields=true".format(material_id))
-
-        if len(result.get("data", [])) > 0:
-            return result
-        else:
-            raise MPRestError("No document found")
