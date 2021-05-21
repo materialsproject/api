6dd8f800dcd84ee048d4dad099cb582ddda9e3a0
Jason Munro <jmunro@lbl.gov>
2021-05-21 15:18:49 -0700

    Model added to similarity client

--- src/mp_api/routes/similarity/client.py
+++ src/mp_api/routes/similarity/client.py
@@ -1,7 +1,9 @@
 from mp_api.core.client import BaseRester
+from mp_api.routes.similarity.models import SimilarityDoc
 
 
 class SimilarityRester(BaseRester):
 
     suffix = "similarity"
+    document_model = SimilarityDoc  # type: ignore
     primary_key = "task_id"
