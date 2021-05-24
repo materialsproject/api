1db96b7f938cd00c445bc4ff739d4f2c4da2a3cd
Jason Munro <jmunro@lbl.gov>
2021-05-24 11:35:56 -0700

    Test files dir added to MAPISettings

--- tests/tasks/test_utils.py
+++ tests/tasks/test_utils.py
@@ -1,12 +1,12 @@
 import os
 from json import load
 
+from mp_api import MAPISettings
 from mp_api.routes.tasks.utils import calcs_reversed_to_trajectory
 
 
 def test_calcs_reversed_to_trajectory():
-    current_dir = os.path.dirname(__file__)
-    with open(os.path.join(current_dir, "../../test_files/calcs_reversed_mp_1031016.json")) as file:
+    with open(os.path.join(MAPISettings().test_files, "calcs_reversed_mp_1031016.json")) as file:
         calcs_reversed = load(file)
         trajectories = calcs_reversed_to_trajectory(calcs_reversed)
 
