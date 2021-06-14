ff8e00c40fc8fbedf5e7344577f37daa758bc977
Jason Munro <jmunro@lbl.gov>
2021-06-14 13:35:08 -0700

    Add proper models for materials

---
+++ src/mp_api/routes/materials/models/doc.py
@@ -0,0 +1,31 @@
+from emmet.core.mpid import MPID
+from pydantic import Field, BaseModel
+
+
+class FindStructure(BaseModel):
+    """
+    Class defining find structure return data
+    """
+
+    task_id: MPID = Field(
+        None,
+        description="The ID of this material, used as a universal reference across property documents."
+        "This comes in the form: mp-******",
+    )
+    normalized_rms_displacement: float = Field(
+        None,
+        description="Volume normalized root-mean squared displacement between the structures",
+    )
+    max_distance_paired_sites: float = Field(
+        None, description="Maximum distance between paired sites",
+    )
+
+
+class FormulaAutocomplete(BaseModel):
+    """
+    Class defining formula autocomplete return data
+    """
+
+    formula_pretty: str = Field(
+        None, description="Human readable chemical formula",
+    )
