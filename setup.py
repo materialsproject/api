e3ee141c05422cf90212b67551da1cf97ea8c04b
Jason Munro <jmunro@lbl.gov>
2021-06-23 13:23:03 -0700

    Bump emmet-core version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         "setuptools",
         "pydantic>=1.4",
-        "pymatgen>=2020.9.14",
+        "pymatgen>=2022.0.9",
         "typing-extensions>=3.7.4.1",
         "requests>=2.23.0",
         "monty",
