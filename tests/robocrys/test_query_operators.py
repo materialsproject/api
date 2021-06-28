98ba61fe7aaa0a4c24772d2fe25b516fe6b183bc
Jason Munro <jmunro@lbl.gov>
2021-06-28 16:10:57 -0700

    Robocrys test added

---
+++ tests/robocrys/test_query_operators.py
@@ -0,0 +1,35 @@
+from mp_api.routes.robocrys.query_operators import RoboTextSearchQuery
+
+
+def test_robocrys_search_query():
+    op = RoboTextSearchQuery()
+
+    pipeline = [
+        {
+            "$search": {
+                "index": "description",
+                "regex": {
+                    "query": ["cubic", "octahedra"],
+                    "path": "description",
+                    "allowAnalyzedField": True,
+                },
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "task_id": 1,
+                "description": 1,
+                "condensed_structure": 1,
+                "last_updates": 1,
+                "search_score": {"$meta": "searchScore"},
+            }
+        },
+        {"$sort": {"search_score": -1}},
+        {"$skip": 0},
+        {"$limit": 10},
+    ]
+
+    assert op.query(keywords="cubic, octahedra", skip=0, limit=10) == {
+        "pipeline": pipeline
+    }
