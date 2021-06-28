497d743b31b116c6b85e6397a423d8db3d8ba0ed
Jason Munro <jmunro@lbl.gov>
2021-06-28 13:48:40 -0700

    Charge density query list change

--- src/mp_api/routes/charge_density/query_operators.py
+++ src/mp_api/routes/charge_density/query_operators.py
@@ -1,4 +1,3 @@
-from typing import List
 from fastapi import Query
 from maggma.api.query_operator.core import QueryOperator
 from maggma.api.utils import STORE_PARAMS
@@ -10,12 +9,22 @@ class ChgcarTaskIDQuery(QueryOperator):
     """
 
     def query(
-        self, task_ids: str = Query(None, description="Comma-separated list of calculation (task) IDs to query on",),
+        self,
+        task_ids: str = Query(
+            None,
+            description="Comma-separated list of calculation (task) IDs to query on",
+        ),
     ) -> STORE_PARAMS:
 
         crit = {}
 
         if task_ids:
-            crit.update({"task_id": {"$in": task_ids.split(",")}})
+            crit.update(
+                {
+                    "task_id": {
+                        "$in": [task_id.strip() for task_id in task_ids.split(",")]
+                    }
+                }
+            )
 
         return {"criteria": crit}
