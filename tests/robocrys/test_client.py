22e3d430ffb279e0fcf106f46b42cccecb0f83c9
Jason Munro <jmunro@lbl.gov>
2021-07-01 14:45:08 -0700

    Robocrys client updates and tests

---
+++ tests/robocrys/test_client.py
@@ -0,0 +1,31 @@
+import pytest
+from mp_api.routes.robocrys.client import RobocrysRester
+
+import inspect
+import typing
+from pymatgen.core.periodic_table import Element
+
+
+@pytest.fixture
+def rester():
+    rester = RobocrysRester()
+    yield rester
+    rester.session.close()
+
+
+def test_client(rester):
+    # Get specific search method
+    search_method = None
+    for entry in inspect.getmembers(rester, predicate=inspect.ismethod):
+        if "search" in entry[0] and entry[0] != "search":
+            search_method = entry[1]
+
+    if search_method is not None:
+
+        q = {"keywords": ["silicon", "process"]}
+
+        doc = search_method(**q)[0]
+
+        assert doc.description is not None
+        assert doc.condensed_structure is not None
+        assert doc.task_id is not None
