73885887d00074ddd32f4b228b3c6e99627469c4
Jason Munro <jmunro@lbl.gov>
2021-07-09 12:49:06 -0700

    Only enable rate limiting for canonical endpoint

--- src/mp_api/core/ratelimit.py
+++ src/mp_api/core/ratelimit.py
@@ -1,11 +1,19 @@
+import os
 from ratelimit import sleep_and_retry, limits
 from mp_api.core.settings import MAPISettings
 
+DEFAULT_ENDPOINT = os.environ.get(
+    "MP_API_ENDPOINT", "https://api.materialsproject.org/"
+)
 
-@sleep_and_retry
-@limits(calls=MAPISettings().requests_per_min, period=60)
+
 def check_limit():
     """
     Empty function for enabling global rate limiting.
     """
     return
+
+
+if "api.materialsproject" in DEFAULT_ENDPOINT:
+    check_limit = limits(calls=MAPISettings().requests_per_min, period=60)(check_limit)
+    check_limit = sleep_and_retry(check_limit)
