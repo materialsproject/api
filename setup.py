70ac15d0cb1c6c3fe97fbdd813d07325f1aad054
Jason Munro <jmunro@lbl.gov>
2021-08-04 12:08:11 -0700

    Bump maggma version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         "requests>=2.23.0",
         "monty",
         "emmet-core",
-        "maggma>=0.30.2",
+        "maggma>=0.30.3",
         "ratelimit",
     ],
     extras_require={
