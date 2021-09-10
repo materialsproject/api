498afdb10ad11ad03f959fab14fcf11f7b39b610
Jason Munro <jmunro@lbl.gov>
2021-09-10 11:05:39 -0700

    Default deprecation query to false and update epts

--- src/mp_api/routes/provenance/resources.py
+++ src/mp_api/routes/provenance/resources.py
@@ -1,5 +1,6 @@
 from maggma.api.resource import ReadOnlyResource
 from maggma.api.query_operator import PaginationQuery
+from mp_api.routes.materials.query_operators import DeprecationQuery
 
 from emmet.core.provenance import ProvenanceDoc
 
@@ -8,7 +9,7 @@ def provenance_resource(provenance_store):
     resource = ReadOnlyResource(
         provenance_store,
         ProvenanceDoc,
-        query_operators=[PaginationQuery()],
+        query_operators=[DeprecationQuery(), PaginationQuery()],
         tags=["Provenance"],
         disable_validation=True,
         enable_default_search=False,
