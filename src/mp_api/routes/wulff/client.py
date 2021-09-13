2fdac2149046720324347d90741151a37e2431c4
Jason Munro <jmunro@lbl.gov>
2021-09-13 11:55:57 -0700

    Remove wulff endpoint

--- src/mp_api/routes/wulff/client.py
+++
@@ -1,9 +0,0 @@
-from mp_api.core.client import BaseRester
-from mp_api.routes.wulff.models import WulffDoc
-
-
-class WulffRester(BaseRester):
-
-    suffix = "wulff"
-    document_model = WulffDoc  # type: ignore
-    primary_key = "task_id"
