7f565285b2524c30b4e7c2eb1ff98754357db323
Matthew Horton <mkhorton@users.noreply.github.com>
2021-10-07 17:14:36 -0700

    Remove all type hint stubs

--- src/mp_api/routes/electrodes/client.pyi
+++
@@ -1,9 +0,0 @@
-from typing import List, Optional
-from emmet.core.electrode import InsertionElectrodeDoc
-
-
-class ElectrodeRester:
-
-    def query_by_task_id(
-        self, task_id, fields: Optional[List[str]] = None, monty_decode: bool = True
-    ) -> InsertionElectrodeDoc: ...
