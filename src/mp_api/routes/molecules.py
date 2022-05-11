09fdbe837ac8c945c7ddc36335921e928fe63505
Jason <jmunro@lbl.gov>
2022-05-11 15:00:41 -0700

    Re-order reqs

--- src/mp_api/routes/molecules.py
+++ src/mp_api/routes/molecules.py
@@ -6,6 +6,7 @@ from pymatgen.core.periodic_table import Element
 from mp_api.core.client import BaseRester
 from emmet.core.molecules_old import MoleculesDoc
 
+
 class MoleculesRester(BaseRester[MoleculesDoc]):
 
     suffix = "molecules"
