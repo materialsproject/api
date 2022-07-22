f8a5257f19c8a702c3171d7d8126b7793d41a8b0
Jason Munro <jmunro@lbl.gov>
2022-07-21 17:28:51 -0700

    Fix pytest import

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -2,6 +2,7 @@ from emmet.core.symmetry import CrystalSystem
 from emmet.core.summary import HasProps
 from pymatgen.analysis.magnetism import Ordering
 from mp_api.routes.summary import SummaryRester
+import pytest
 
 import typing
 
