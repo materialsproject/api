6a6d72006118ba43659eff7bfaff7c08e5d2ec98
Jason Munro <jason.munro@gmail.com>
2022-10-10 18:00:06 -0700

    Skip chgcar test

--- tests/test_mprester.py
+++ tests/test_mprester.py
@@ -277,6 +277,7 @@ class TestMPRester:
         dos = mpr.get_phonon_dos_by_material_id("mp-11659")
         assert isinstance(dos, PhononDos)
 
+    @pytest.mark.skip(reason="Test needs fixing with ENV variables")
     def test_get_charge_density_data(self, mpr):
         chgcar = mpr.get_charge_density_from_material_id("mp-149")
         assert isinstance(chgcar, Chgcar)
