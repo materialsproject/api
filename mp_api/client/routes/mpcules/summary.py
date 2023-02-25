7c09cfb8b19123e2c05bbb412a528e2db9dc0c92
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2023-02-25 10:35:38 -0800

    Beginning MPcules summary rester, modeled off of materials summary rester

---
+++ mp_api/client/routes/mpcules/summary.py
@@ -0,0 +1,212 @@
+import warnings
+from collections import defaultdict
+from typing import List, Optional, Tuple, Union
+
+from emmet.core.mpid import MPID
+from emmet.core.molecules.summary import HasProps, SummaryDoc
+
+from mp_api.client.core import BaseRester
+from mp_api.client.core.utils import validate_ids
+
+
+class MPculeSummaryRester(BaseRester[SummaryDoc]):
+
+    suffix = "summary"
+    document_model = SummaryDoc  # type: ignore
+    primary_key = "molecule_id"
+
+    def search(
+        self,
+        charge: Optional[Tuple[int, int]] = None,
+        spin_multiplicity: Optional[Tuple[int, int]] = None,
+        nelements: Optional[Tuple[int, int]] = None,
+        has_solvent: Optional[Union[str, List[str]]] = None,
+        has_level_of_theory: Optional[Union[str, List[str]]] = None,
+        has_lot_solvent: Optional[Union[str, List[str]]] = None,
+        with_solvent: Optional[str] = None,
+        electronic_energy: Optional[Tuple[float, float]] = None,
+        ionization_energy: Optional[Tuple[float, float]] = None,
+        electron_affinity: Optional[Tuple[float, float]] = None,
+        reduction_free_energy: Optional[Tuple[float, float]] = None,
+        oxidation_free_energy: Optional[Tuple[float, float]] = None,
+        # zero_point_energy: Optional[Tuple[float, float]] = None,
+        # total_enthalpy: Optional[Tuple[float, float]] = None,
+        # total_entropy: Optional[Tuple[float, float]] = None,
+        # translational_enthalpy: Optional[Tuple[float, float]] = None,
+        # translational_entropy: Optional[Tuple[float, float]] = None,
+        # vibrational_enthalpy: Optional[Tuple[float, float]] = None,
+        # vibrational_entropy: Optional[Tuple[float, float]] = None,
+        # rotational_enthalpy: Optional[Tuple[float, float]] = None,
+        # rotational_entropy: Optional[Tuple[float, float]] = None,
+        # free_energy: Optional[Tuple[float, float]] = None,
+        chemsys: Optional[Union[str, List[str]]] = None,
+        deprecated: Optional[bool] = None,
+        elements: Optional[List[str]] = None,
+        exclude_elements: Optional[List[str]] = None,
+        formula: Optional[Union[str, List[str]]] = None,
+        has_props: Optional[List[HasProps]] = None,
+        material_ids: Optional[List[MPID]] = None,
+        num_elements: Optional[Tuple[int, int]] = None,
+        num_sites: Optional[Tuple[int, int]] = None,
+        sort_fields: Optional[List[str]] = None,
+        num_chunks: Optional[int] = None,
+        chunk_size: int = 1000,
+        all_fields: bool = True,
+        fields: Optional[List[str]] = None,
+    ):
+        """
+        Query core data using a variety of search criteria.
+
+        Arguments:
+            charge (Tuple[int, int]): Minimum and maximum charge for the molecule.
+            spin_multiplicity (Tuple[int, int]): Minimum and maximum spin for the molecule.
+            nelements (Tuple[int, int]): Minimum and maximum number of elements
+            chemsys (str, List[str]): A chemical system, list of chemical systems
+                (e.g., Li-C-O, [C-O-H-N, Li-N]), or single formula (e.g., C2 H4).
+            has_solvent (str, List[str]): Whether the molecule has properties calculated in
+                solvents (e.g., SOLVENT=THF, [SOLVENT=WATER, VACUUM])
+            # TODO: continue documentation
+            deprecated (bool): Whether the material is tagged as deprecated.
+            elements (List[str]): A list of elements.
+            exclude_elements (List(str)): List of elements to exclude.
+            formula (str, List[str]): A formula including anonymized formula
+                or wild cards (e.g., Fe2O3, ABO3, Si*). A list of chemical formulas can also be passed
+                (e.g., [Fe2O3, ABO3]).
+            has_props: (List[HasProps]): The calculated properties available for the material.
+            material_ids (List[MPID]): List of Materials Project IDs to return data for.
+            num_elements (Tuple[int,int]): Minimum and maximum number of elements to consider.
+            num_sites (Tuple[int,int]): Minimum and maximum number of sites to consider.
+            sort_fields (List[str]): Fields used to sort results. Prefixing with '-' will sort in descending order.
+            num_chunks (int): Maximum number of chunks of data to yield. None will yield all possible.
+            chunk_size (int): Number of data entries per chunk.
+            all_fields (bool): Whether to return all fields in the document. Defaults to True.
+            fields (List[str]): List of fields in SearchDoc to return data for.
+                Default is material_id if all_fields is False.
+
+        Returns:
+        """
+
+        query_params = defaultdict(dict)  # type: dict
+
+        min_max_name_dict = {
+            "total_energy": "energy_per_atom",
+            "formation_energy": "formation_energy_per_atom",
+            "energy_above_hull": "energy_above_hull",
+            "uncorrected_energy": "uncorrected_energy_per_atom",
+            "equilibrium_reaction_energy": "equilibrium_reaction_energy_per_atom",
+            "nsites": "nsites",
+            "volume": "volume",
+            "density": "density",
+            "band_gap": "band_gap",
+            "efermi": "efermi",
+            "total_magnetization": "total_magnetization",
+            "total_magnetization_normalized_vol": "total_magnetization_normalized_vol",
+            "total_magnetization_normalized_formula_units": "total_magnetization_normalized_formula_units",
+            "num_magnetic_sites": "num_magnetic_sites",
+            "num_unique_magnetic_sites": "num_unique_magnetic_sites",
+            "k_voigt": "k_voigt",
+            "k_reuss": "k_reuss",
+            "k_vrh": "k_vrh",
+            "g_voigt": "g_voigt",
+            "g_reuss": "g_reuss",
+            "g_vrh": "g_vrh",
+            "elastic_anisotropy": "universal_anisotropy",
+            "poisson_ratio": "homogeneous_poisson",
+            "e_total": "e_total",
+            "e_ionic": "e_ionic",
+            "e_electronic": "e_electronic",
+            "n": "n",
+            "num_sites": "nsites",
+            "num_elements": "nelements",
+            "piezoelectric_modulus": "e_ij_max",
+            "weighted_surface_energy": "weighted_surface_energy",
+            "weighted_work_function": "weighted_work_function",
+            "surface_energy_anisotropy": "surface_anisotropy",
+            "shape_factor": "shape_factor",
+        }
+
+        for param, value in locals().items():
+            if param in min_max_name_dict and value:
+                if isinstance(value, (int, float)):
+                    value = (value, value)
+                query_params.update(
+                    {
+                        f"{min_max_name_dict[param]}_min": value[0],
+                        f"{min_max_name_dict[param]}_max": value[1],
+                    }
+                )
+
+        if material_ids:
+            query_params.update({"material_ids": ",".join(validate_ids(material_ids))})
+
+        if deprecated is not None:
+            query_params.update({"deprecated": deprecated})
+
+        if formula:
+            if isinstance(formula, str):
+                formula = [formula]
+
+            query_params.update({"formula": ",".join(formula)})
+
+        if chemsys:
+            if isinstance(chemsys, str):
+                chemsys = [chemsys]
+
+            query_params.update({"chemsys": ",".join(chemsys)})
+
+        if elements:
+            query_params.update({"elements": ",".join(elements)})
+
+        if exclude_elements is not None:
+            query_params.update({"exclude_elements": ",".join(exclude_elements)})
+
+        if possible_species is not None:
+            query_params.update({"possible_species": ",".join(possible_species)})
+
+        query_params.update(
+            {
+                "crystal_system": crystal_system,
+                "spacegroup_number": spacegroup_number,
+                "spacegroup_symbol": spacegroup_symbol,
+            }
+        )
+
+        if is_stable is not None:
+            query_params.update({"is_stable": is_stable})
+
+        if is_gap_direct is not None:
+            query_params.update({"is_gap_direct": is_gap_direct})
+
+        if is_metal is not None:
+            query_params.update({"is_metal": is_metal})
+
+        if magnetic_ordering:
+            query_params.update({"ordering": magnetic_ordering.value})
+
+        if has_reconstructed is not None:
+            query_params.update({"has_reconstructed": has_reconstructed})
+
+        if has_props:
+            query_params.update({"has_props": ",".join([i.value for i in has_props])})
+
+        if theoretical is not None:
+            query_params.update({"theoretical": theoretical})
+
+        if sort_fields:
+            query_params.update(
+                {"_sort_fields": ",".join([s.strip() for s in sort_fields])}
+            )
+
+        query_params = {
+            entry: query_params[entry]
+            for entry in query_params
+            if query_params[entry] is not None
+        }
+
+        return super()._search(
+            num_chunks=num_chunks,
+            chunk_size=chunk_size,
+            all_fields=all_fields,
+            fields=fields,
+            **query_params,
+        )
