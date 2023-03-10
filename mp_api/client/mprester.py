b66b4726aac83621f5d0aad78f15151bc52e5453
Ryan Kingsbury <rkingsbury@users.noreply.github.com>
2023-03-10 12:04:32 -0800

    get_ion_reference_data_for_chemsys: fix bug in str chemsys

--- mp_api/client/mprester.py
+++ mp_api/client/mprester.py
@@ -749,7 +749,9 @@ class MPRester:
         """
 
         ion_data = self.get_ion_reference_data()
-
+        
+        if isinstance(chemsys, str):
+            chemsys = chemsys.split("-")
         return [d for d in ion_data if d["data"]["MajElements"] in chemsys]
 
     def get_ion_entries(self, pd: PhaseDiagram, ion_ref_data: List[dict] = None) -> List[IonEntry]:
