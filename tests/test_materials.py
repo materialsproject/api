cc6a798c4719447ce234520d1a61555cabf1ce3b
Jason Munro <jason.munro@gmail.com>
2023-03-17 13:43:54 -0700

    Revert exclude elements inclusion

--- tests/test_materials.py
+++ tests/test_materials.py
@@ -19,8 +19,8 @@ excluded_params = [
     "num_chunks",
     "all_fields",
     "fields",
+    "exclude_elements",  # temp until server timeout increase
 ]
-
 
 sub_doc_fields = []  # type: list
 
@@ -32,6 +32,7 @@ alt_name_dict = {
     "spacegroup_symbol": "symmetry",
     "exclude_elements": "material_id",
     "num_elements": "nelements",
+    "exclude_elements": "elements",
     "num_sites": "nsites",
 }  # type: dict
 
@@ -40,7 +41,6 @@ custom_field_tests = {
     "formula": "Si",
     "chemsys": "Si-O",
     "elements": ["Si", "O"],
-    "excluded_elements": ["Si", "O"],
     "task_ids": ["mp-149"],
     "crystal_system": CrystalSystem.cubic,
     "spacegroup_number": 38,
@@ -66,14 +66,14 @@ def test_client(rester):
                 if param_type == typing.Tuple[int, int]:
                     project_field = alt_name_dict.get(param, None)
                     q = {
-                        param: (-10, 10),
+                        param: (-100, 100),
                         "chunk_size": 1,
                         "num_chunks": 1,
                     }
                 elif param_type == typing.Tuple[float, float]:
                     project_field = alt_name_dict.get(param, None)
                     q = {
-                        param: (-10.12, 10.12),
+                        param: (-100.12, 100.12),
                         "chunk_size": 1,
                         "num_chunks": 1,
                     }
@@ -92,12 +92,10 @@ def test_client(rester):
                         "num_chunks": 1,
                     }
 
-                project_field = project_field or param
-
-                doc = search_method(**{**q, "fields": project_field})[0].dict()
+                doc = search_method(**q)[0].dict()
 
                 for sub_field in sub_doc_fields:
                     if sub_field in doc:
                         doc = doc[sub_field]
 
-                assert doc[project_field] is not None
+                assert doc[project_field if project_field is not None else param] is not None
