5324d52d6e456373b177e936d6355fa560ac3318
Jason <jmunro@lbl.gov>
2023-11-07 17:44:51 -0800

    Fix structure test

--- tests/test_mprester.py
+++ tests/test_mprester.py
@@ -46,7 +46,7 @@ class TestMPRester:
         assert s1.formula == "Ca12 Ti8 O28"
 
         s1 = mpr.get_structure_by_material_id("mp-149", final=False)
-        assert [s.formula for s in s1] == ["Si2"]
+        assert {s.formula for s in s1} == {"Si2"}
 
         # # requesting via task-id instead of mp-id
         with pytest.warns(UserWarning):
