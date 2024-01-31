8f3d95df9120945cf98c72ec6da6f8185175baee
munrojm <jmunro@lbl.gov>
2024-01-31 11:50:32 -0800

    Add search method for DOI rester

--- mp_api/client/routes/materials/doi.py
+++ mp_api/client/routes/materials/doi.py
@@ -1,18 +1,43 @@
 from __future__ import annotations
 
+from collections import defaultdict
+
 from emmet.core.dois import DOIDoc
 
 from mp_api.client.core import BaseRester
+from mp_api.client.core.utils import validate_ids
 
 
 class DOIRester(BaseRester[DOIDoc]):
     suffix = "doi"
     document_model = DOIDoc  # type: ignore
-    primary_key = "task_id"
+    primary_key = "material_id"
 
-    def search(*args, **kwargs):  # pragma: no cover
-        raise NotImplementedError(
-            """
-            The DOIRester.search method does not exist as no search endpoint is present. Use get_data_by_id instead.
-            """
+    def search(
+        self,
+        material_ids: str | list[str] | None = None,
+        num_chunks: int | None = None,
+        chunk_size: int = 1000,
+        all_fields: bool = True,
+        fields: list[str] | None = None,
+    ):
+        query_params = defaultdict(dict)  # type: dict
+        if material_ids:
+            if isinstance(material_ids, str):
+                material_ids = [material_ids]
+
+            query_params.update({"material_ids": ",".join(validate_ids(material_ids))})
+
+        query_params = {
+            entry: query_params[entry]
+            for entry in query_params
+            if query_params[entry] is not None
+        }
+
+        return super()._search(
+            num_chunks=num_chunks,
+            chunk_size=chunk_size,
+            all_fields=all_fields,
+            fields=fields,
+            **query_params,
         )
