abb78ea119c51f26af16335c7756f29175a7448b
kberket <kberket@lbl.gov>
2025-08-01 11:02:18 -0700

    add test for large result test on robocrys search

--- tests/materials/test_robocrys.py
+++ tests/materials/test_robocrys.py
@@ -23,3 +23,18 @@ def test_client(rester):
 
         assert doc.description is not None
         assert doc.condensed_structure is not None
+
+
+# TODO: switch this to the skipif once emmet PR 1261 is deployed to production
+# @pytest.mark.skipif(os.getenv("MP_API_KEY", None) is None, reason="No API key found.")
+@pytest.mark.skip(reason="Query with large result set fails with faceted pipeline")
+def test_client_large_result_set(rester):
+    search_method = rester.search
+
+    if search_method is not None:
+        q = {"keywords": ["Orthorhombic"], "num_chunks": 1}
+
+        doc = search_method(**q)[0]
+
+        assert doc.description is not None
+        assert doc.condensed_structure is not None
