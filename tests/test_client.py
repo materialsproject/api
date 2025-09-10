2747d8cb9b7b6b38c6f402cc46cd1fd5c41dd0cc
Patrick Huck <phuck@lbl.gov>
2025-09-09 17:17:11 -0700

    rm print

--- tests/test_client.py
+++ tests/test_client.py
@@ -82,5 +82,4 @@ def test_generic_get_methods(rester):
 
         elif name not in special_resters:
             doc = rester.get_data_by_id(key_only_resters[name], fields=[key])
-            print(doc.model_dump())
             assert isinstance(doc, rester.document_model)
