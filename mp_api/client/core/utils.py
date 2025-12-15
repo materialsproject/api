438878e930de1e4ec56f0b43cb0af7057771cb9c
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-12-15 14:28:17 -0800

    remove allow_msonable_dict as its only prior use was in api_sanitize, also removed

--- mp_api/client/core/utils.py
+++ mp_api/client/core/utils.py
@@ -88,33 +88,3 @@ def validate_ids(id_list: list[str]):
     if validate_identifier:
         return [str(validate_identifier(idx)) for idx in id_list]
     return _legacy_id_validation(id_list)
-
-
-def allow_msonable_dict(monty_cls: type[MSONable]):
-    """Patch Monty to allow for dict values for MSONable."""
-
-    def validate_monty(cls, v, _):
-        """Stub validator for MSONable as a dictionary only."""
-        if isinstance(v, cls):
-            return v
-        elif isinstance(v, dict):
-            # Just validate the simple Monty Dict Model
-            errors = []
-            if v.get("@module", "") != monty_cls.__module__:
-                errors.append("@module")
-
-            if v.get("@class", "") != monty_cls.__name__:
-                errors.append("@class")
-
-            if len(errors) > 0:
-                raise ValueError(
-                    "Missing Monty seriailzation fields in dictionary: {errors}"
-                )
-
-            return v
-        else:
-            raise ValueError(f"Must provide {cls.__name__} or MSONable dictionary")
-
-    monty_cls.validate_monty_v2 = classmethod(validate_monty)
-
-    return monty_cls
