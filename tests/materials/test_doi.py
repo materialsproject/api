50730264b31f6bc9095298ef45afa476cd53dd55
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-12-16 13:01:08 -0800

    with context

--- tests/materials/test_doi.py
+++ tests/materials/test_doi.py
@@ -5,20 +5,21 @@ from ..conftest import client_search_testing, requires_api_key
 
 @requires_api_key
 def test_doi_search():
-    client_search_testing(
-        search_method=DOIRester().search,
-        excluded_params=[
-            "num_chunks",
-            "chunk_size",
-            "all_fields",
-            "fields",
-        ],
-        alt_name_dict={
-            "material_ids": "material_id",
-        },
-        custom_field_tests={
-            "material_ids": ["mp-149", "mp-13"],
-            "material_ids": "mp-149",
-        },
-        sub_doc_fields=[],
-    )
+    with DOIRester() as rester:
+        client_search_testing(
+            search_method=rester.search,
+            excluded_params=[
+                "num_chunks",
+                "chunk_size",
+                "all_fields",
+                "fields",
+            ],
+            alt_name_dict={
+                "material_ids": "material_id",
+            },
+            custom_field_tests={
+                "material_ids": ["mp-149", "mp-13"],
+                "material_ids": "mp-149",
+            },
+            sub_doc_fields=[],
+        )
--- tests/materials/test_phonon.py
+++ tests/materials/test_phonon.py
@@ -13,24 +13,25 @@ from ..conftest import client_search_testing, requires_api_key
 
 @requires_api_key
 def test_phonon_search():
-    client_search_testing(
-        search_method=PhononRester().search,
-        excluded_params=[
-            "num_chunks",
-            "chunk_size",
-            "all_fields",
-            "fields",
-        ],
-        alt_name_dict={
-            "material_ids": "material_id",
-        },
-        custom_field_tests={
-            "material_ids": ["mp-149", "mp-13"],
-            "material_ids": "mp-149",
-            "phonon_method": "dfpt",
-        },
-        sub_doc_fields=[],
-    )
+    with PhononRester() as rester:
+        client_search_testing(
+            search_method=rester.search,
+            excluded_params=[
+                "num_chunks",
+                "chunk_size",
+                "all_fields",
+                "fields",
+            ],
+            alt_name_dict={
+                "material_ids": "material_id",
+            },
+            custom_field_tests={
+                "material_ids": ["mp-149", "mp-13"],
+                "material_ids": "mp-149",
+                "phonon_method": "dfpt",
+            },
+            sub_doc_fields=[],
+        )
 
 
 @requires_api_key
