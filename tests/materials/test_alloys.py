c97e9bbf3fd070644b10345221eefedc5196ab28
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-12-15 22:11:02 -0800

    basic alloys tests

---
+++ tests/materials/test_alloys.py
@@ -0,0 +1,30 @@
+import os
+
+import pytest
+
+from mp_api.client.routes.materials.alloys import AlloysRester
+
+from core_function import client_search_testing
+
+
+@pytest.mark.skipif(os.getenv("MP_API_KEY") is None, reason="No API key found.")
+def test_alloys_search():
+    client_search_testing(
+        search_method=AlloysRester().search,
+        excluded_params=[
+            "num_chunks",
+            "chunk_size",
+            "all_fields",
+            "fields",
+        ],
+        alt_name_dict={
+            "material_ids": "pair_id",
+            "formulae": "alloy_pair",
+        },
+        custom_field_tests={
+            "material_ids": ["mp-70", "mp-1014212"],
+            "material_ids": "mp-1014212",
+            "formulae": ["Rb", "Si"],
+        },
+        sub_doc_fields=[],
+    )
