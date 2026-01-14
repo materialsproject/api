5e3289d9dced3ae1b95cd8f0e8f9b629662cbe56
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2026-01-14 14:17:31 -0800

    more more housekeeping

--- tests/molecules/test_molecules.py
+++ tests/molecules/test_molecules.py
@@ -1,10 +1,19 @@
-"""Test basic molecules functionality."""
+"""Test basic molecules functionality.
+
+This class currently is non-functional, except as an access
+point for other resters, therefore we include only basic tests here.
+"""
 
 from mp_api.client.routes.molecules.molecules import BaseMoleculeRester, MoleculeRester
 from mp_api.client.routes.molecules import MOLECULES_RESTERS
 
 
 def test_molecule_rester():
-    assert set(dir(MoleculeRester())) == set(
-        dir(BaseMoleculeRester) + list(MOLECULES_RESTERS)
-    )
+    with MoleculeRester() as rester:
+        assert set(dir(rester)) == set(
+            dir(BaseMoleculeRester) + list(MOLECULES_RESTERS)
+        )
+
+        assert all(
+            getattr(rester, k) == lazy_obj for k, lazy_obj in MOLECULES_RESTERS.items()
+        )
