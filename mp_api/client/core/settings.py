09547579f9530d8ffd2172b23b45e3b8c1aa6ae1
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2026-06-05 14:08:10 -0700

    stabilized, back to main endpoint

--- mp_api/client/core/settings.py
+++ mp_api/client/core/settings.py
@@ -21,7 +21,7 @@ _MUTE_PROGRESS_BAR = PMG_SETTINGS.get("MPRESTER_MUTE_PROGRESS_BARS", False)
 _MAX_LIST_LENGTH = min(PMG_SETTINGS.get("MPRESTER_MAX_LIST_LENGTH", 10000), 10000)
 
 _EMMET_SETTINGS = EmmetSettings()  # type: ignore[call-arg]
-_DEFAULT_ENDPOINT = "https://api-preview.materialsproject.org/"
+_DEFAULT_ENDPOINT = "https://api.materialsproject.org/"
 
 
 class MAPIClientSettings(BaseSettings):
