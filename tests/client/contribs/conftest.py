51739273756735b71a4e26648779e1d4e5009c50
Tyler Mathis <35553152+tsmathis@users.noreply.github.com>
2026-06-15 12:30:58 -0700

    updated pytest compat

--- tests/client/contribs/conftest.py
+++ tests/client/contribs/conftest.py
@@ -1,11 +1,34 @@
+import pytest
+from packaging.version import Version
+
 try:
     from mp_api.client.contribs import ContribsClient
 except ImportError:
     ContribsClient = None
 
 
-def pytest_ignore_collect(path, config):
-    # Skip tests if contribs client isn't installed
-    if ContribsClient is None and "contribs" in str(path):
-        return True
-    return False
+_pytest_version = Version(pytest.__version__)
+PYTEST_GE_8_0 = any(
+    [
+        _pytest_version.is_devrelease,
+        _pytest_version.is_prerelease,
+        _pytest_version >= Version("8.0"),
+    ]
+)
+
+
+if PYTEST_GE_8_0:
+
+    def pytest_ignore_collect(collection_path, config):
+        # Skip tests if contribs client isn't installed
+        if ContribsClient is None and "contribs" in str(collection_path):
+            return True
+        return False
+
+else:
+
+    def pytest_ignore_collect(path, config):
+        # Skip tests if contribs client isn't installed
+        if ContribsClient is None and "contribs" in str(path):
+            return True
+        return False
